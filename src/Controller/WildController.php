b98e5c985b2ebd446bec89a0881f558b55a96de4
cedricrouxcedric <cedricroux@outlook.fr>
2019-11-23 11:09:03 +0100

    first part quest 8 done

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -2,6 +2,7 @@
 // src/Controller/WildController.php
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,30 +13,55 @@ use Symfony\Component\Routing\Annotation\Route;
 class WildController extends AbstractController
 {
     /**
+     * Show all rows from Program’s entity
+     *
      * @Route("/index", name="index")
+     * @return Response A response instance
      */
     public function index(): Response
     {
-        return $this->render('wild/index.html.twig', [
-            'website' => 'Wild Séries',
-        ]);
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No program found in program\'s table.'
+            );
+        }
+
+        return $this->render(
+            'wild/index.html.twig',
+            ['programs' => $programs]
+        );
     }
     /**
+     * Getting a program with a formatted slug for title
+     *
      * @param string $slug The slugger
      * @Route("/show/{slug<^[a-z0-9-]+$>}", defaults={"slug" = null}, name="show")
      * @return Response
      */
     public function show(?string $slug): Response
-    {
-
-        if(!$slug)
-        {
-            $slug=('Aucune série sélectionnée veuillez choisir une série') ;
-        }
-        $slug = preg_replace('/-/',' ', ucwords(trim(strip_tags($slug)), "-"));
-
-
-
-        return $this->render('wild/show.html.twig', ['slug' => $slug]);
+    {if (!$slug) {
+        throw $this
+            ->createNotFoundException('No slug has been sent to find a program in program\'s table.');
     }
-}
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with '.$slug.' title, found in program\'s table.'
+            );
+        }
+
+        return $this->render('wild/show.html.twig', [
+            'program' => $program,
+            'slug'  => $slug,
+        ]);
+}}
